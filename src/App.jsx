817e05612923c0db0e2959f18fd4ac68a9521ec4
Rishita Ingle <48147126+rishita17ingle@users.noreply.github.com>
2020-05-12 15:12:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.scss';
+import { Login, Register } from "./Components/login/index";
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoginActive: true,
+    }
+  }
+
+  changeState() {
+    const {isLoginActive} = this.state;
+    if(isLoginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    }
+    else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+
+    this.setState((prevState) => ({isLoginActive: !prevState.isLoginActive}));
+  }
+
+
+  render() {
+    const { isLoginActive } = this.state;
+    const current = isLoginActive ? "Register" : "Login";   
+    const currentActive = isLoginActive ? "login" : "register";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container">
+            {isLoginActive && <Login containerRef ={ (ref) => this.current=ref}/>}
+            {!isLoginActive && <Register  containerRef ={ (ref) => this.current=ref}/>}
+          </div>
+          <RightSide current={current} containerRef={ref => this.rightSide = ref} onClick={this.changeState.bind(this)}/>
+        </div>
+      </div>
+
+    );
+  }
+
+  
+}
+
+const RightSide = props => {
+  return (<div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default App;
