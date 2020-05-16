5590f6a571c4e34c6d29ecd2aede5ed8f4dba4fc
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-05-16 19:16:37 +0530

    drop down added

--- src/Components/login/register.jsx
+++ src/Components/login/register.jsx
@@ -35,6 +35,15 @@ import {connect} from 'react-redux';
                         <input type="email" name="email" ref={(input)=>this.email=input}placeholder="email"/>
                     </div>
                     <div className="form-group">
+                        <label htmlFor="email">User</label>
+                        <select id = "myList">
+               <option value = "1">one</option>
+               <option value = "2">two</option>
+               <option value = "3">three</option>
+               <option value = "4">four</option>
+             </select>
+                        </div>
+                    <div className="form-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" name="password" ref={(input)=>this.password=input}placeholder="password"/>
                     </div>
